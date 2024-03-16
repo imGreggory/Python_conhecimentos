9d6bd553a3b9d2b9a437cd00097d381698e5f1f8
imGreggory <anthony.greggory03@gmail.com>
2024-03-15 22:57:03 -0300

    Atualização teste_merge

--- estrutura_de_dados/aula-tuplas.py
+++ estrutura_de_dados/aula-tuplas.py
@@ -2,5 +2,5 @@
 lanche = "Hamburguer"
 print(lanche)
 #exemplo 2 - variáveis compostas (tuplas)
-lanchetupla = "Hamburguer", "Suco", "Pizza", "Pudim", "Picolé", "Hot-Dog"
+lanchetupla = "Hamburguer", "Suco", "Pizza", "Pudim", "Picolé", "Lasanha"
 print(lanchetupla[1:3])
