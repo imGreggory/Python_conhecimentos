0b521ce06adf173ef0f081b109a1bc21955a531d
imGreggory <anthony.greggory03@gmail.com>
2024-03-26 10:21:42 -0300

    primeiro exercicio function

--- funções/exercicio01.py
+++ funções/exercicio01.py
@@ -3,7 +3,6 @@ numeros = []
 def amplitude (maior, menor):
     return maior - menor
 
-
 def main():
     for i in range (0,10):
         numero = round(random.uniform(1,50),0)
@@ -11,6 +10,5 @@ def main():
     print(f"A lista de números é: {numeros}")
     maior = max(numeros)
     menor = min(numeros)
-    amplitude(maior,menor)
-    print(f"A amplitude dessa lista é de {amplitude(maior,menor)}")
+    print(f"A amplitude dessa lista é de {amplitude(maior, menor)}")
 main()
